de772920996c66b12b6a01e4057f059201291a12
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2021-10-01 15:11:22 +0700

    Menyelesaikan video 6 tentang memasukan Assets

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends Controller{
     public function index()
     {
         $data['judul'] = 'Home';
-        $this->view('templates/header');
+        $this->view('templates/header', $data);
         $this->view('home/index');
         $this->view('templates/footer');
     }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,7 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-5">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/jagad.jpg" alt="Jagaddhita Arya Koswara" width="200" class="rounded-circle shadow">
     <p>
         Halo Nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>
     </p>
+</div>
